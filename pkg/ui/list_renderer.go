2c9d483f4cdafb44db2d806ecc9c66c24d0764cd
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-05-07 23:55:17 -0400

    fix: fixed list renderer

--- pkg/ui/list_renderer.go
+++ pkg/ui/list_renderer.go
@@ -23,7 +23,7 @@ func (lr *ListRenderer) increment() {
 	if lr.selected >= lr.listSize-1 {
 		return
 	}
-	lr.setSelected(lr.selected + 1)
+	lr.selected++
 	if lr.selected >= lr.endIdx {
 		lr.endIdx++
 		lr.startIdx++
@@ -34,7 +34,7 @@ func (lr *ListRenderer) decrement() {
 	if lr.selected <= 0 {
 		return
 	}
-	lr.setSelected(lr.selected - 1)
+	lr.selected--
 	if lr.selected < lr.startIdx {
 		lr.endIdx--
 		lr.startIdx--
@@ -42,7 +42,25 @@ func (lr *ListRenderer) decrement() {
 }
 
 func (lr *ListRenderer) setSelected(idx int) {
+	if idx < 0 || idx > lr.listSize {
+		return
+	}
+
+	size := min(lr.renderSize, lr.listSize)
 	lr.selected = idx
+	lr.startIdx = min(lr.selected, lr.listSize-size)
+	lr.endIdx = lr.startIdx + size
+}
+
+func (lr *ListRenderer) setListSize(listSize int) {
+	lr.listSize = listSize
+	if listSize == 0 {
+		lr.setSelected(0)
+	} else if lr.selected >= listSize {
+		lr.setSelected(listSize - 1)
+	} else {
+		lr.setSelected(lr.selected)
+	}
 }
 
 func min(a int, b int) int {
@@ -52,16 +70,11 @@ func min(a int, b int) int {
 	return b
 }
 
-func (lr *ListRenderer) setListSize(listSize int) {
-	newEnd := min(listSize, lr.renderSize)
-	if lr.selected >= newEnd {
-		lr.selected = newEnd - 1
+func max(a int, b int) int {
+	if a > b {
+		return a
 	}
-	if lr.selected < 0 {
-		lr.setSelected(0)
-	}
-	lr.endIdx = newEnd
-	lr.listSize = listSize
+	return b
 }
 
 func (lr *ListRenderer) forEach(f func(idx int)) {
