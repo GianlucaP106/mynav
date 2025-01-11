af106c8ddc64a7b4b303890ae26bbdc8831c7ed1
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2025-01-11 15:47:56 -0500

    feat: report duration attached in session (#413)

--- pkg/system/time.go
+++ pkg/system/time.go
@@ -20,53 +20,48 @@ func UnixTime(t string) time.Time {
 }
 
 func TimeAgo(t time.Time) string {
-	duration := time.Since(t)
+	timeStr := TimeDeltaStr(time.Since(t))
+	if timeStr == "0 seconds" {
+		return "now"
+	}
+	return timeStr + " ago"
+}
+
+func TimeDeltaStr(duration time.Duration) string {
 	switch {
 	case duration < time.Minute:
 		seconds := int(duration.Seconds())
 		if seconds == 0 {
-			return "now"
+			return "0 seconds"
 		}
 
 		if seconds == 1 {
-			return "1 second ago"
+			return "1 second"
 		}
-		return fmt.Sprintf("%d seconds ago", seconds)
+		return fmt.Sprintf("%d seconds", seconds)
 	case duration < time.Hour:
 		minutes := int(duration.Minutes())
 		if minutes == 1 {
-			return "1 minute ago"
+			return "1 minute"
 		}
-		return fmt.Sprintf("%d minutes ago", minutes)
+		return fmt.Sprintf("%d minutes", minutes)
 	case duration < 24*time.Hour:
 		hours := int(duration.Hours())
 		if hours == 1 {
-			return "1 hour ago"
+			return "1 hour"
 		}
-		return fmt.Sprintf("%d hours ago", hours)
+		return fmt.Sprintf("%d hours", hours)
 	case duration < 30*24*time.Hour:
 		days := int(duration.Hours() / 24)
 		if days == 1 {
-			return "1 day ago"
+			return "1 day"
 		}
-		return fmt.Sprintf("%d days ago", days)
+		return fmt.Sprintf("%d days", days)
 	default:
 		months := int(duration.Hours() / (24 * 30))
 		if months == 1 {
-			return "1 month ago"
+			return "1 month"
 		}
-		return fmt.Sprintf("%d months ago", months)
+		return fmt.Sprintf("%d months", months)
 	}
 }
-
-// func main() {
-// 	// Example timestamp
-// 	timestamp := time.Now().Add(-45 * time.Second) // 45 seconds ago
-// 	fmt.Println(timeAgo(timestamp))
-//
-// 	timestamp = time.Now().Add(-3 * time.Hour) // 3 hours ago
-// 	fmt.Println(timeAgo(timestamp))
-//
-// 	timestamp = time.Now().Add(-7 * 24 * time.Hour) // 7 days ago
-// 	fmt.Println(timeAgo(timestamp))
-// }
